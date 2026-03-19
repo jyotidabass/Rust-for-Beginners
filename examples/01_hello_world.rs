4a75706db3fca16777d9b84d41771098c98bd928
Jyoti Dabass, Ph.D <34393429+jyotidabass@users.noreply.github.com>
2026-03-19 15:28:23 +0530

    Add files via upload

---
+++ examples/01_hello_world.rs
@@ -0,0 +1,8 @@
+// Example 1: Hello World
+// This is the most basic Rust program
+// Run with: rustc 01_hello_world.rs && ./01_hello_world
+
+fn main() {
+    println!("Hello, World!");
+    println!("Welcome to Rust programming!");
+}
