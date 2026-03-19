03e058f616f37a13e8be642d4c9962028dc659ae
Jyoti Dabass, Ph.D <34393429+jyotidabass@users.noreply.github.com>
2026-03-19 15:28:47 +0530

    Delete examples  directory

--- examples /01_hello_world.rs
+++
@@ -1,8 +0,0 @@
-// Example 1: Hello World
-// This is the most basic Rust program
-// Run with: rustc 01_hello_world.rs && ./01_hello_world
-
-fn main() {
-    println!("Hello, World!");
-    println!("Welcome to Rust programming!");
-}
